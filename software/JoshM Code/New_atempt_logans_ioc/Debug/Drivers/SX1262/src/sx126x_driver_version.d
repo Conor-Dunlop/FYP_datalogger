8c3e4d3f9e5d5688cc775cd009b1a6ad4767b427
jmc670 <jmc670@uclive.ac.nz>
2024-09-15 22:02:21 +1200

    SX1262 SPI testing

--- software/JoshM Code/New_atempt_logans_ioc/Debug/Drivers/SX1262/src/sx126x_driver_version.d
+++
@@ -1,4 +0,0 @@
-Drivers/SX1262/src/sx126x_driver_version.o: \
- ../Drivers/SX1262/src/sx126x_driver_version.c \
- ../Drivers/SX1262/src/sx126x_driver_version.h
-../Drivers/SX1262/src/sx126x_driver_version.h:
